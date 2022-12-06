d3393f66470b8dda7c77a1a3b9b42f6cd7d62f3a
cymmgithub <przemswiercz@gmail.com>
2022-12-06 02:15:30 +0100

    make it better

--- dist/routers/hall-of-fame.js
+++ dist/routers/hall-of-fame.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,11 +8,13 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { Router } from "express";
-import * as path from "path";
-import { PilotRecord } from "../records/pilot.record";
-export const hallOfFameRouter = Router();
-hallOfFameRouter
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hallOfFameRouter = void 0;
+const express_1 = require("express");
+const path = require("path");
+const pilot_record_1 = require("../records/pilot.record");
+exports.hallOfFameRouter = (0, express_1.Router)();
+exports.hallOfFameRouter
     .get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res
         .sendFile('hall-of-fame.html', {
@@ -19,7 +22,7 @@ hallOfFameRouter
     });
 }))
     .get('/top-mechs', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const topPilots = yield PilotRecord.listTop(8);
+    const topPilots = yield pilot_record_1.PilotRecord.listTop(8);
     const data = topPilots.map(pilot => {
         return {
             name: pilot.pilotName,
--- dist/routers/pilot.js
+++ dist/routers/pilot.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,12 +8,14 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import { Router } from 'express';
-import * as path from 'path';
-import { PilotRecord } from '../records/pilot.record';
-import { ValidationError } from '../utils/error';
-export const PilotRouter = Router();
-PilotRouter
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PilotRouter = void 0;
+const express_1 = require("express");
+const path = require("path");
+const pilot_record_1 = require("../records/pilot.record");
+const error_1 = require("../utils/error");
+exports.PilotRouter = (0, express_1.Router)();
+exports.PilotRouter
     .get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     res
         .sendFile('pilot-configurator.html', {
@@ -23,16 +26,16 @@ PilotRouter
     const { playerId, winnerCookie } = req.cookies;
     if (winnerCookie) {
         const timeLeft = (winnerCookie.createdAt + 10) - new Date().getTime() / 1000;
-        throw new ValidationError(`Your Mech is still resting after last fight. Wait ${timeLeft} s.`);
+        throw new error_1.ValidationError(`Your Mech is still resting after last fight. Wait ${timeLeft} s.`);
     }
-    const randomOpponent = yield PilotRecord.getRandom(playerId);
+    const randomOpponent = yield pilot_record_1.PilotRecord.getRandom(playerId);
     randomOpponent.enemy = true;
     res
         .cookie('enemyId', randomOpponent.id)
         .json(randomOpponent);
 }))
     .post('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const newPilot = new PilotRecord(Object.assign(Object.assign({}, req.body), { strength: req.body.strength, defense: req.body.defense, stamina: req.body.stamina, agility: req.body.agility }));
+    const newPilot = new pilot_record_1.PilotRecord(Object.assign(Object.assign({}, req.body), { strength: req.body.strength, defense: req.body.defense, stamina: req.body.stamina, agility: req.body.agility }));
     yield newPilot.insert();
     res
         .cookie('playerId', newPilot.id)
