3e948054a0c3c71a8a58527da50824505e1709e4
cymmgithub <przemswiercz@gmail.com>
2022-12-06 09:33:24 +0100

    make it better

--- dist/routers/hall-of-fame.js
+++ dist/routers/hall-of-fame.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 import { Router } from "express";
 import * as path from "path";
-import { PilotRecord } from "../records/pilot.record";
+import { PilotRecord } from "../records/pilot.record.js";
 export const hallOfFameRouter = Router();
 hallOfFameRouter
     .get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
